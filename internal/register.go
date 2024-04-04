4d81a0e8fd8fd0e9324076593b42cd634b7ff960
Andrey Karpov <ndk@users.noreply.github.com>
2024-04-04 10:40:58 +0200

    wip

--- internal/register.go
+++ internal/register.go
@@ -1,10 +1,20 @@
 package internal
 
 import (
+	"github.com/ankorstore/yokai/config"
+	"github.com/labstack/echo/v4"
+	"github.com/ndk/test123/internal/handler"
+	"github.com/ndk/test123/spec"
 	"go.uber.org/fx"
 )
 
 // Register is used to register the application dependencies.
 func Register() fx.Option {
-	return fx.Options()
+	return fx.Options(
+		fx.Invoke(func(e *echo.Echo) {
+			a := handler.NewAPI(&config.Config{})
+			strictApiHandler := spec.NewStrictHandler(a, nil)
+			spec.RegisterHandlers(e, strictApiHandler)
+		}),
+	)
 }
